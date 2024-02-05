0849811c5156f94a00db0af59ec9be51f24f26e0
mavia-ali-dd <109381633+mavia-ali-dd@users.noreply.github.com>
2024-02-05 18:15:48 +0500

    grouping test

--- src/components/Greet/greet.test.js
+++ src/components/Greet/greet.test.js
@@ -7,8 +7,32 @@ test("greet renders", () => {
     expect(element).toBeInTheDocument();
 })
 
-test("greet renders with name", () => {
-    render(<Greet name="Name" />);
-    const element = screen.getByText("Greetings Name");
-    expect(element).toBeInTheDocument();
+describe("Greet", () => {
+    test("greet renders", () => {
+        render(<Greet />);
+        const element = screen.getByText("Greetings");
+        expect(element).toBeInTheDocument();
+    })
+
+    test("greet renders with name", () => {
+        render(<Greet name="Name" />);
+        const element = screen.getByText("Greetings Name");
+        expect(element).toBeInTheDocument();
+    })
+})
+
+describe.skip("Greet", () => {
+    test("greet renders", () => {
+        render(<Greet />);
+        const element = screen.getByText("Greetings");
+        expect(element).toBeInTheDocument();
+    })
+
+    describe('Nested', () => {
+        test("greet renders with name", () => {
+            render(<Greet name="Name" />);
+            const element = screen.getByText("Greetings Name");
+            expect(element).toBeInTheDocument();
+        })
+    })
 })
